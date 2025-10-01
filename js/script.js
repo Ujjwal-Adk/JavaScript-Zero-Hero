ab7b80d9f0a480ac47f51024305e04edf81cb6db
Ujjwal-Adk <ujjwal.official010@gmail.com>
2025-10-01 13:56:12 -0600

    Declaring_variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log(" Hello World !")
+console.log(" Hello World !");
+console.log("My portfolio is now powered by JavaScript.")
+
+// So now This is Day 2 and lets learn about the variables today 
+// there are three ways to declare the variables i.e let , var , const 
+// best practice to declare any variables is using let alwasys 
+
+const myName  = "Ujjwal Adhikari"; // String 
+const myAge = 20; // int 
+
+let currentYear = 2025; //int
+let isLearning = true; //Boolean
+
+// Now as we learned declaring variables we will print that in console 
+
+console.log(myName)
+console.log("I am" + myAge + "years old");
+console.log("current Year :" + currentYear)
+
